7949c505c088efffce0ccd0ec69408d91169a927
HARISH.D <HARISH.D@192.168.0.8>
2020-09-21 15:51:33 +0530

    Saving the proj to Remote

---
+++ AmazonAutmateProject/src/test/java/amazeAutomatePack/AutomateTest1.java
@@ -0,0 +1,20 @@
+package amazeAutomatePack;
+import java.util.*;
+
+public class AutomateTest1 {
+
+	public static void main(String[] args) {
+		
+		
+		int[] arr1= {22,46,22,44,22,46,54,68,46,68,22,44,86,88,89,85,88,84};
+		TreeSet<Integer> set=new TreeSet<Integer>();
+		
+		  for(int i:arr1)
+		{
+			set.add(i);
+		}
+		System.out.println(set);
+	
+	}
+
+}
---
+++ AmazonAutmateProject/src/test/java/amazeAutomatePack/LoginTest.java
@@ -0,0 +1,44 @@
+package amazeAutomatePack;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	WebDriver driver;
+
+	@BeforeClass
+public void openbrowser()
+{
+	System.setProperty("webdriver.gecko.driver","F:\\BROWSERFILES\\geckodriver.exe");
+	FirefoxOptions options = new FirefoxOptions();
+	options.setCapability("marionette", false);
+	driver = new FirefoxDriver(options);
+	driver.get("https://www.facebook.com/");
+}
+	@Test
+	public void openfb() throws InterruptedException
+	{
+		driver.get("https://www.facebook.com/");
+		System.out.println(driver.getTitle());		
+		driver.manage().timeouts().implicitlyWait(2,TimeUnit.SECONDS);
+		driver.findElement(By.id("email")).sendKeys("TestEmail");
+		driver.findElement(By.xpath(".//input[@id='pass']")).sendKeys("TestEmail");
+		driver.findElement(By.id("u_0_b")).click();
+		Thread.sleep(3000);
+		System.out.println(driver.getTitle());
+		
+	}
+	@AfterClass
+	public void endTest()
+	{
+		driver.quit();
+	}
+
+}
